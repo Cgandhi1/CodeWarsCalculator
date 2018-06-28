2f5b752ebab151168c93b4532561e3e59f9c98d1
Chirag Gandhi <chiraggandhi18@gmail.com>
2018-06-28 15:11:37 -0500

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = "2 / 2 + 3 * 4 - 6";
+            expression = Regex.Replace(expression, @"\s+", "");
+            DataTable MyTable = new DataTable();
+            DataColumn MyColumn = new DataColumn();
+            MyColumn.ColumnName = "MyColumn";
+            MyColumn.Expression = expression;
+            MyColumn.DataType = typeof(double);
+            MyTable.Columns.Add(MyColumn);
+            DataRow MyRow = MyTable.NewRow();
+            MyTable.Rows.Add(MyRow);
+            double num = (double)(MyTable.Rows[0]["MyColumn"]);
+            num = Math.Truncate(1000000 * num) / 1000000;
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
